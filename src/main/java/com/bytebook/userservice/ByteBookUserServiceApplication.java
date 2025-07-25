8f16fc845f13c54c6c581a2c41cc941002bda3e0
Evgeniy Siganov <EvgeniySiganov>
2025-07-25 09:11:07 +0300

    [init]

---
+++ src/main/java/com/bytebook/userservice/ByteBookUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bytebook.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBookUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBookUserServiceApplication.class, args);
+	}
+
+}
